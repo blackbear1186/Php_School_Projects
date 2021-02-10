5faf12766a55dfdc4d59aea403130f8c8d1c3f48
Berlin Johnson <blackbear@Berlins-MacBook-Pro.local>
2021-02-10 19:27:27 +0900

    Completed update file and fixed add movie function in index.php

--- Movies-forms/index.php
+++ Movies-forms/index.php
@@ -20,8 +20,8 @@ if($action === 'list-movies'){
   $movies = getAllMovies();
   $pageTitle = 'List Movies';
   include 'view/movies_list.php';
-}
-else if($action === 'show-add-movie'){
+
+} else if($action === 'show-add-movie'){
     $pageTitle = 'Add Movies';
     include 'view/movie_add.php';
 }
@@ -40,11 +40,40 @@ else if($action === 'add-movie'){
         include 'view/movie_add.php';
     }
     else {
-        addMovie($movieTitle, $movieGenre, $movieRating, $releaseYear, $imdbScore);
+        addMovie($movieTitle, $movieGenre, $releaseYear, $movieRating, $imdbScore);
         $movies = getAllMovies();
         $pageTitle = 'List Movies';
         include 'view/movies_list.php';
     }
+} else if($action === 'show-update-movie'){
+    $id = filter_input(INPUT_POST, 'ID', FILTER_SANITIZE_NUMBER_INT);
+    $movie = getMovieInfo($id);
+    $pageTitle = 'Update Movie';
+    include 'view/movie_update.php';
+
+} else if($action === 'update-movie'){
+    $id = filter_input(INPUT_POST, 'ID', FILTER_SANITIZE_NUMBER_INT);
+    $movieTitle = filter_input(INPUT_POST, 'movie-title', FILTER_SANITIZE_STRING);
+    $movieGenre = filter_input(INPUT_POST, 'movie-genre', FILTER_SANITIZE_STRING);
+    $releaseYear = filter_input(INPUT_POST, 'release-year', FILTER_SANITIZE_NUMBER_INT);
+    $movieRating = filter_input(INPUT_POST, 'movie-rating');
+    $imdbScore = filter_input(INPUT_POST, 'imdb-score', FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+
+    // if any of the input values are empty display error message
+    if(!strlen($movieTitle) || !strlen($movieGenre) || !strlen($releaseYear) || !strlen($imdbScore)){
+        $error = 'All fields in the Update form must contain data. Please ensure all form elements contain appropriate values.';
+        $movie = getMovieInfo($id);
+        $pageTitle = 'Update Movie';
+        include 'view/movie_update.php';
+    } else {
+        updateMovie($id, $movieTitle, $movieGenre, $releaseYear, $movieRating, $imdbScore);
+        $movies = getAllMovies();
+        $pageTitle = 'List Movies';
+        include 'view/movies_list.php';
+    }
+}
+else if($action === 'clear-message'){
+    header('Location:.');
 }
 else {
   $error = "The <strong>$action</strong> action was not handled in the code.";
--- Movies-forms/model/movies_db.php
+++ Movies-forms/model/movies_db.php
@@ -41,3 +41,46 @@ function addMovie($movieTitle, $movieGenre, $releaseYear, $movieRating, $imdbSco
         $successMessage = 'The movie <strong>' . $movieTitle . '</strong> was successfully added to the database.';
     }
 }
+function getMovieInfo($id){
+    global $db;
+    global $error;
+    $query = 'SELECT * FROM Movies WHERE ID = :Movie_id';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':Movie_id', $id, PDO::PARAM_INT);
+    $statement->execute();
+    $movie = $statement->fetch();
+    $statement->closeCursor();
+    if($statement->errorCode() !== 0 && empty($movie)){
+        $sqlError = $statement->errorInfo();
+        $error = 'SELECT error &rarr; The movie with the ID <strong>' . $id . '</strong> was not retrieved because: ' . $sqlError[2];
+    }
+    return $movie;
+}
+function updateMovie($id, $movieTitle, $movieGenre, $releaseYear, $movieRating, $imdbScore){
+    global $db;
+    global $error, $successMessage;
+    $query = 'UPDATE Movies
+                SET ID = :Movie_id,
+                    MovieTitle = :MovieTitle,
+                    MovieGenre = :MovieGenre,
+                    ReleaseYear = :ReleaseYear,
+                    MovieRating = :MovieRating,
+                    IMDB_Score = :IMDB_Score
+                WHERE ID = :Movie_id';
+    $statement = $db->prepare($query);
+    $statement->bindValue('Movie_id', $id, PDO::PARAM_INT);
+    $statement->bindValue(':MovieTitle', $movieTitle);
+    $statement->bindValue(':MovieGenre', $movieGenre);
+    $statement->bindValue(':ReleaseYear', $releaseYear);
+    $statement->bindValue(':MovieRating', $movieRating);
+    $statement->bindValue(':IMDB_Score', $imdbScore);
+    $success = $statement->execute();
+    $statement->closeCursor();
+    if($statement->errorCode() !== 0 && $success === false){
+        $sqlError = $statement->errorInfo();
+        $error = 'Update error &rarr; The movie <strong>' . $movieTitle . '</strong> was not updated because: ' . $sqlError[2];
+    }
+    else {
+        $successMessage = 'The movie <strong>' . $movieTitle . '</strong> was successfully updated.';
+    }
+}
