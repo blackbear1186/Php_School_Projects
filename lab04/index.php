8564e8b9a5e7cda1d36fb7d2eaae603b065c78a1
Berlin Johnson <blackbear@Berlins-MacBook-Pro.local>
2021-02-11 12:03:58 +0900

    Update Home is Good

--- lab04/index.php
+++ lab04/index.php
@@ -33,7 +33,8 @@ if($action === 'list-homes'){
     $homeSize = filter_input(INPUT_POST, 'home-size', FILTER_SANITIZE_NUMBER_INT);
     $lotSize = filter_input(INPUT_POST, 'lot-size', FILTER_SANITIZE_NUMBER_INT);
     $homePrice = filter_input(INPUT_POST, 'home-price', FILTER_SANITIZE_NUMBER_INT);
-    if(!strlen($homeTitle) || !strlen($homeAddress) || !strlen($homeCity) || !strlen($homeState) || !strlen($zipCode) || !strlen($homeBeds) || !strlen($homeBaths) ||
+
+    if(!strlen($homeTitle) || !strlen($homeAddress) || !strlen($homeCity) || $homeState === 'choose' || !strlen($zipCode) || $homeBeds === 'choose' || $homeBaths === 'choose' ||
     !strlen($homeSize) || !strlen($lotSize) || !strlen($homePrice)){
         $error = 'All fields in the Add form must contain data. Please ensure all form elements contain appropriate values.';
         $pageTitle = 'Add Home';
@@ -45,6 +46,37 @@ if($action === 'list-homes'){
         include 'view/realEstate_list.php';
     }
 
+} else if($action === 'show-update-home'){
+    $id = filter_input(INPUT_POST, 'ID', FILTER_SANITIZE_NUMBER_INT);
+    $home = getHomeInfo($id);
+    $pageTitle = 'Update Home';
+    include 'view/realEstate_update.php';
+
+} else if($action === 'update-home'){
+    $id = filter_input(INPUT_POST, 'ID', FILTER_SANITIZE_NUMBER_INT);
+    $homeTitle = filter_input(INPUT_POST, 'home-title', FILTER_SANITIZE_STRING);
+    $homeAddress = filter_input(INPUT_POST, 'home-address', FILTER_SANITIZE_STRING);
+    $homeCity = filter_input(INPUT_POST, 'home-city', FILTER_SANITIZE_STRING);
+    $homeState = filter_input(INPUT_POST, 'home-state');
+    $zipCode = filter_input(INPUT_POST, 'zip-code', FILTER_SANITIZE_NUMBER_INT);
+    $homeBeds = filter_input(INPUT_POST, 'home-beds');
+    $homeBaths = filter_input(INPUT_POST, 'home-baths');
+    $homeSize = filter_input(INPUT_POST, 'home-size', FILTER_SANITIZE_NUMBER_INT);
+    $lotSize = filter_input(INPUT_POST, 'lot-size', FILTER_SANITIZE_NUMBER_INT);
+    $homePrice = filter_input(INPUT_POST, 'home-price', FILTER_SANITIZE_NUMBER_INT);
+
+    if(!strlen($homeTitle) || !strlen($homeAddress) || !strlen($homeCity) || !strlen($zipCode) || !strlen($homeSize) || !strlen($lotSize) || !strlen($homePrice)){
+        $error = 'All fields in the Add form must contain data. Please ensure all form elements contain appropriate values.';
+        $home = getHomeInfo($id);
+        $pageTitle = 'Update Home';
+        include 'view/realEstate_update.php';
+
+    } else {
+        addHome($homeTitle, $homeAddress, $homeCity, $homeState, $zipCode, $homeBeds, $homeBaths, $homeSize, $lotSize, $homePrice);
+        $realEstate = getAllRealEstate();
+        $pageTitle = 'List Homes';
+        include 'view/realEstate_list.php';
+    }
 }
 else {
   $error = "The <strong>$action</strong> action was not handled in the code.";
--- lab04/model/realEstate_db.php
+++ lab04/model/realEstate_db.php
@@ -43,7 +43,7 @@ function addHome($homeTitle, $homeAddress, $homeCity, $homeState, $zipCode, $hom
 function getHomeInfo($id){
     global $db;
     global $error;
-    $query = 'SELECT * FROM real_estate WHERE ID = :Home_ID';
+    $query = 'SELECT * FROM real_estate WHERE ID = :Home_id';
     $statement = $db->prepare($query);
     $statement->bindValue(':Home_id', $id, PDO::PARAM_INT);
     $statement->execute();
@@ -54,4 +54,41 @@ function getHomeInfo($id){
         $error = 'SELECT error &rarr; The home with ID <strong>' . $id . '</strong> was not retrieved because: ' . $sqlError[2];
     }
     return $home;
+}
+function updateHome($homeTitle, $homeAddress, $homeCity, $homeState, $zipCode, $homeBeds, $homeBaths, $homeSize, $lotSize, $homePrice){
+    global $db;
+    global $error, $successMessage;
+    $query = 'UPDATE real_estate 
+                SET ID = :Home_id,
+                    Title = :Title,
+                    Address = :Address,
+                    City = :City,
+                    State = :State,
+                    Zip = :Zip,
+                    Beds = :Beds,
+                    Baths = :Baths,
+                    HouseSize = :HouseSize,
+                    LotSize = :LotSize,
+                    Price = :Price
+                WHERE ID = :Home_id';
+    $statement = $db->prepare($query);
+    $statement->bindValue('Home_id', $id, PDO::PARAM_INT);
+    $statement->bindValue(':Title', $homeTitle);
+    $statement->bindValue(':Address', $homeAddress);
+    $statement->bindValue(':City', $homeCity);
+    $statement->bindValue(':State', $homeState);
+    $statement->bindValue(':Zip', $zipCode);
+    $statement->bindValue(':Beds', $homeBeds);
+    $statement->bindValue(':Baths', $homeBaths);
+    $statement->bindValue(':HouseSize', $homeSize);
+    $statement->bindValue(':LotSize', $lotSize);
+    $statement->bindValue(':Price', $homePrice);
+    $success = $statement->execute();
+    $statement->closeCursor();
+    if($statement->errorCode() !== 0 && $success === false){
+        $sqlError = $statement->errorInfo();
+        $error = 'Update error &rarr; The home <strong>' . $homeTitle . '</strong> was not updated because: ' . $sqlError[2];
+    } else {
+        $successMessage = 'The home <strong> ' . $homeTitle . '</strong> was successfully updated.';
+    }
 }
