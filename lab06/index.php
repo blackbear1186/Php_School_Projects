940ef0d4926289f02108cab111934a9777c201b7
Berlin Johnson <berlin.johnson@protonmail.com>
2021-03-05 10:13:56 +0900

    Fixed html validation error

--- lab06/index.php
+++ lab06/index.php
@@ -1,6 +1,6 @@
 <?php include 'functions.php';?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
