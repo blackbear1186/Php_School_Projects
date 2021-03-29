d103d46fdbfb92c673d0337c219b0cadf2a9e821
Berlin Johnson <berlin.johnson@protonmail.com>
2021-03-29 19:53:00 +0900

    Changes to database.php

---
+++ lab08/model/database.php
@@ -0,0 +1,29 @@
+<?php
+class Database {
+    private static $dsn = 'mysql:host=localhost;dbname=webdsuco_berlin375';
+    private static $username = 'webdsuco_berlin375';
+    private static $password = 'webdsuco_berlin375';
+    private static $db;
+
+    public function __construct()
+    {
+    }
+
+    public static function getDB() {
+        if(!isset(self::$db)) {
+            try {
+                self::$db = new PDO(self::$dsn, self::$username, self::$password);
+            } catch (PDOException $e) {
+                $errorMessage = $e->getMessage();
+                include 'view/header.php';
+                echo '<h1>Database Error</h1>';
+                echo '<p>' . $errorMessage . '</p>';
+                include 'view/footer.php';
+                exit();
+            }
+        }
+        return self::$db;
+    }
+}
+
+
---
+++ lab08/model/realEstate_db.php
@@ -0,0 +1,117 @@
+<?php
+function getAllRealEstate(){
+  global $db;
+  global $error;
+  $query = 'SELECT * FROM real_estate ORDER BY Title';
+  $statement = $db->prepare($query);
+  $statement->execute();
+  $realEstate = $statement->fetchAll();
+  $statement->closeCursor();
+  if($statement->errorCode() !== 0 && empty($realEstate)){
+    $sqlError = $statement->errorInfo();
+    $error = 'SELECT error &rarr; The houses were not retrieved because: ' . $sqlError[2];
+  }
+  return $realEstate;
+}
+function addHome($homeTitle, $homeAddress, $homeCity, $homeState, $zipCode, $homeBeds, $homeBaths, $homeSize, $lotSize, $homePrice){
+    global $db;
+    global $error, $successMessage;
+    $query = 'INSERT INTO real_estate
+                (Title, Address, City, State, Zip, Beds, Baths, HouseSize, LotSize, Price)
+                VALUES
+                (:Title, :Address, :City, :State, :Zip, :Beds, :Baths, :HouseSize, :LotSize, :Price)';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':Title', $homeTitle);
+    $statement->bindValue(':Address', $homeAddress);
+    $statement->bindValue(':City', $homeCity);
+    $statement->bindValue(':State', $homeState);
+    $statement->bindValue(':Zip', $zipCode);
+    $statement->bindValue(':Beds', $homeBeds);
+    $statement->bindValue(':Baths', $homeBaths);
+    $statement->bindValue(':HouseSize', $homeSize);
+    $statement->bindValue(':LotSize', $lotSize);
+    $statement->bindValue(':Price', $homePrice);
+    $success = $statement->execute();
+    $statement->closeCursor();
+    if($statement->errorCode() !== 0 && $success === false){
+        $sqlError = $statement->errorInfo();
+        $error = 'INSERT error &rarr; The home <strong>' . $homeTitle . '</strong> was not added because: ' . $sqlError[2];
+        logErrorMessage($error);
+    } else {
+        $successMessage = 'The home <strong> ' . $homeTitle . '</strong> was successfully added.';
+        logSuccessMessage($successMessage);
+    }
+}
+function getHomeInfo($id){
+    global $db;
+    global $error;
+    $query = 'SELECT * FROM real_estate WHERE ID = :Home_id';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':Home_id', $id, PDO::PARAM_INT);
+    $statement->execute();
+    $home = $statement->fetch();
+    $statement->closeCursor();
+    if($statement->errorCode() !== 0 && empty($home)){
+        $sqlError = $statement->errorInfo();
+        $error = 'SELECT error &rarr; The home with ID <strong>' . $id . '</strong> was not retrieved because: ' . $sqlError[2];
+        logErrorMessage($error);
+    }
+    return $home;
+}
+function updateHome($id, $homeTitle, $homeAddress, $homeCity, $homeState, $zipCode, $homeBeds, $homeBaths, $homeSize, $lotSize, $homePrice){
+    global $db;
+    global $error, $successMessage;
+    $query = 'UPDATE real_estate 
+                SET ID = :Home_id,
+                    Title = :Title,
+                    Address = :Address,
+                    City = :City,
+                    State = :State,
+                    Zip = :Zip,
+                    Beds = :Beds,
+                    Baths = :Baths,
+                    HouseSize = :HouseSize,
+                    LotSize = :LotSize,
+                    Price = :Price
+                WHERE ID = :Home_id';
+    $statement = $db->prepare($query);
+    $statement->bindValue('Home_id', $id, PDO::PARAM_INT);
+    $statement->bindValue(':Title', $homeTitle);
+    $statement->bindValue(':Address', $homeAddress);
+    $statement->bindValue(':City', $homeCity);
+    $statement->bindValue(':State', $homeState);
+    $statement->bindValue(':Zip', $zipCode);
+    $statement->bindValue(':Beds', $homeBeds);
+    $statement->bindValue(':Baths', $homeBaths);
+    $statement->bindValue(':HouseSize', $homeSize);
+    $statement->bindValue(':LotSize', $lotSize);
+    $statement->bindValue(':Price', $homePrice);
+    $success = $statement->execute();
+    $statement->closeCursor();
+    if($statement->errorCode() !== 0 && $success === false){
+        $sqlError = $statement->errorInfo();
+        $error = 'Update error &rarr; The home <strong>' . $homeTitle . '</strong> was not updated because: ' . $sqlError[2];
+        logErrorMessage($error);
+    } else {
+        $successMessage = 'The home <strong> ' . $homeTitle . '</strong> was successfully updated.';
+        logSuccessMessage($successMessage);
+    }
+}
+function deleteHome($id, $homeTitle){
+    global $db;
+    global $error, $successMessage;
+
+    $query = 'DELETE FROM real_estate WHERE ID = :Home_id';
+    $statement = $db->prepare($query);
+    $statement->bindValue('Home_id', $id, PDO::PARAM_INT);
+    $success = $statement->execute();
+    $statement->closeCursor();
+    if($statement->errorCode() !== 0 && $success === false){
+        $sqlError = $statement->errorInfo();
+        $error = 'DELETE error &rarr; The home <strong>' . $homeTitle . '</strong> was not deleted because: ' . $sqlError[2];
+        logErrorMessage($error);
+    } else {
+        $successMessage = 'The home <strong> ' . $homeTitle . '</strong> was successfully deleted.';
+        logSuccessMessage($successMessage);
+    }
+}
